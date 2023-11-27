2e31dd75d88be9879067d41dba4f1fc5e98d75bb
Вахитов Тимур <vt@greentechs.pro>
2023-11-27 16:10:01 +0300

    1 lesson routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Home from "./Components/home/Home";
+import {Route, Routes} from "react-router-dom";
+import LoginPage from "./Components/auth/LoginPage"
+import PrivateRoute from "./utils/router/PrivateRoute";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home/>}/>
+        </Route>
+        <Route path="login" element={<LoginPage/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/auth/LoginPage.js
@@ -0,0 +1,11 @@
+import './LoginPage.css';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h1 className='heading'>Login Page</h1>
+        </div>
+    )
+}
+
+export default LoginPage;
